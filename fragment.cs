bf8c48b18f7bbc335eab7c6f2b3bc7cdba81c5b6
Ali Koba <avk013@gmail.com>
2016-03-18 21:28:56 +0200

    возможность указывать начальную нумерацию
    возможность указывать начальную нумерацию при вызове инициализации

--- fragment.cs
+++ fragment.cs
@@ -1,9 +1,8 @@
-private void dginit(DataGridView datagridname, int max_column, int max_row)
-{int min = 0;//0;
-datagridname.Rows.Clear();
-datagridname.Columns.Clear();
-int stro = 0;
-for (int i = min; i <=max_column; i++) datagridname.Columns.Add("Column", i.ToString());
-for (int i = min; i < max_row; i++) datagridname.Rows.Add();
-for (int i = min; i <= max_row; i++) datagridname.Rows[stro++].HeaderCell.Value = i.ToString();
-datagridname.AutoResizeColumns();}
+ private void dginit(DataGridView datagridname, int max_column, int max_row, int begin_num=0)
+ {datagridname.Rows.Clear();
+ datagridname.Columns.Clear();
+ int stro = 0;
+ for (int i = begin_num; i <= max_column; i++) datagridname.Columns.Add("Column", i.ToString());
+ for (int i = begin_num; i < max_row; i++) datagridname.Rows.Add();
+ for (int i = begin_num; i <= max_row; i++) datagridname.Rows[stro++].HeaderCell.Value = i.ToString();
+ datagridname.AutoResizeColumns();}
